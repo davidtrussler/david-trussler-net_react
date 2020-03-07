c4916a0280d1733b8da9d2f820e483493975c374
David Trussler <mail@davidtrussler.net>
2020-03-07 20:25:28 +0000

    Creates components for main sections - Adds Header component to App
    - Adds Main component to App
    
    - Adds Footer component to App
    
    - Adds fragment container

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export class Header extends Component {
+	render = () => 
+		<header>
+			<div className="constrained">
+			  <div className="header__inner">
+			    <h1 className="header__title">David Trussler</h1>
+			  </div>
+			</div>
+		</header>
+}
